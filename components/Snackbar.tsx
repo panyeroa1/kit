bb672ab9db0b6e662b8a23f66db6ad71f03531f2
Emil Danguilan <emilalvaro@aitekchat.com>
2025-10-05 05:44:24 +0800

    refactor(deps): Update UI state import path
    Updated the import path for `useUI` from `../../lib/state` to `@/lib/state` to align with the new project structure.

--- components/Snackbar.tsx
+++ components/Snackbar.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
 */
 import React, { useEffect } from 'react';
-import { useUI } from '../../lib/state';
+import { useUI } from '@/lib/state';
 import cn from 'classnames';
 
 const SNACKBAR_TIMEOUT = 4000;
